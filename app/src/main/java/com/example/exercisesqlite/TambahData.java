21cbb5ed449c39fc2220b0436bfcec4ee8509bf6
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-15 14:58:12 +0700

    Declare editText in class TambahData & find by id

--- app/src/main/java/com/example/exercisesqlite/TambahData.java
+++ app/src/main/java/com/example/exercisesqlite/TambahData.java
@@ -3,12 +3,20 @@ package com.example.exercisesqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class TambahData extends AppCompatActivity {
+
+    EditText etNama, etPhone, etEmail, etAlamat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data);
+
+        etNama   = (EditText) findViewById(R.id.addnama);
+        etPhone  = (EditText) findViewById(R.id.addNoTelp);
+        etEmail  = (EditText) findViewById(R.id.addEmail);
+        etAlamat = (EditText) findViewById(R.id.addAlamat);
     }
 }
