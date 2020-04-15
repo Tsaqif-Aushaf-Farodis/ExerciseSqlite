0cf3fe8b056d95113410b5511a6de463ed7b5702
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-04-15 15:25:04 +0700

    Create function for listView

--- app/src/main/java/com/example/exercisesqlite/MainActivity.java
+++ app/src/main/java/com/example/exercisesqlite/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView listView;
+    DBHelper mydb;
     FloatingActionButton fab;
 
     @Override
@@ -22,6 +30,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent (getApplicationContext(), TambahData.class);
                 startActivity(i);
+            }
+        });
+
+        mydb = new DBHelper(this);
+        ArrayList arrayList = mydb.getAllContacts();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView = (ListView) findViewById(R.id.lv1);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                //TODO Auto-generated method stub
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(), TambahData.class);
+                intent.putExtras(dataBundle);
+                startActivity(intent);
             }
         });
     }
